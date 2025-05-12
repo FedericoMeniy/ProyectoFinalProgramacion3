0f911a2c838b865255a05ec0cf5b761c90d37093
ManueldelValle <139931547+ManueldelValle@users.noreply.github.com>
2025-05-12 09:09:55 -0300

    Add files via upload

---
+++ src/main/java/com/Meniy_DelValle_Jordan_Acuna_Lopez/proyecto_final_prog3/ProyectoFinalProg3Application.java
@@ -0,0 +1,13 @@
+package com.Meniy_DelValle_Jordan_Acuna_Lopez.proyecto_final_prog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalProg3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalProg3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Meniy_DelValle_Jordan_Acuna_Lopez/proyecto_final_prog3/ProyectoFinalProg3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Meniy_DelValle_Jordan_Acuna_Lopez.proyecto_final_prog3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalProg3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
